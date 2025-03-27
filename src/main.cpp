de2a2ceb03a2039b5186fa4062884ee76c3e3251
Finn <github.ikarulus@zer.one>
2025-03-27 11:35:28 +0100

    baro code now functioning, recv and sender

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "gps_driver.h"
+#include <Adafruit_BMP085.h>
+#include <SPI.h>
 #include <esp_now.h>
 #include <WiFi.h>
 #include <Wire.h>
-#include <Adafruit_BMP085.h>
+
 
 //Connection Baro
 //SDA	A4
@@ -10,10 +12,12 @@
 
 #define MEAN_HUBLAND_SL_ALTITUDE 275 //Calibration Altitude for SL pressure
 
+Adafruit_BMP085 bmp;
+
 int counter = 0;
 
 int initial_sea_l_press = -1;
-int initial_baro_altitude = -1;
+float initial_baro_altitude = -1.0;
 
 // Receiver MAC Address
 uint8_t broadcastAddress[] = {0x9c, 0x9c, 0x1f, 0xc4, 0xc4, 0x94};
@@ -23,32 +27,13 @@ typedef struct struct_message {
     char latitude[15];
     char longitude[15];
     char altitude[10];
+    float baro_alt;
 } struct_message;
 
 // Data packages
 struct_message myData;
 
 esp_now_peer_info_t peerInfo;
-
-//Baro ->
-
-void setup() {
-    Serial.begin(9600);
-
-}
-
-void loop() {
-    Serial.print("Temperatur = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-
-    Serial.print("Luftdruck = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-
-    Serial.println();
-    delay(2000);
-}
 
 // ESP-NOW: callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
@@ -58,23 +43,23 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 void setup() {
     // Set device as a Wi-Fi Station
     WiFi.mode(WIFI_STA);
-    
+
     // Init ESP-NOW
     if (esp_now_init() != ESP_OK) {
         Serial.println("Error initializing ESP-NOW");
         return;
     }
-    
+
     // Once ESPNow is successfully Init, we will register for Send CB to
     // get the status of Trasnmitted packet
     esp_now_register_send_cb(OnDataSent);
-    
+
     // Register peer
     memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-    peerInfo.channel = 0;  
+    peerInfo.channel = 0;
     peerInfo.encrypt = false;
-    
-    // Add peer        
+
+    // Add peer
     if (esp_now_add_peer(&peerInfo) != ESP_OK){
         Serial.println("Failed to add peer");
         return;
@@ -89,13 +74,14 @@ void setup() {
         delay(500);
         while (1) {}
     }
-    initial_sea_l_press = readSealevelPressure(HUBLAND_SL_ALTITUDE);
-    initial_baro_altitude = readAltitude(initial_sea_l_press);
+    initial_sea_l_press = bmp.readSealevelPressure(MEAN_HUBLAND_SL_ALTITUDE);
+    initial_baro_altitude = bmp.readAltitude(initial_sea_l_press);
 }
 
 void loop() {
     gps_update();
-    int baro_altitude = readAltitude(initial_sea_l_press);
+    float baro_altitude = bmp.readAltitude(initial_sea_l_press);
+    float baro_diff = baro_altitude - initial_baro_altitude;
     Serial.print("\r🌍 Lat: ");
     Serial.print(gps_get_latitude());
     Serial.print(" | Lon: ");
@@ -103,13 +89,14 @@ void loop() {
     Serial.print(" | 🏔 Alt: ");
     Serial.print(gps_get_altitude());
     Serial.print("m    BaroDiff: ");
-    Serial.print(baro_altitude - initial_baro_altitude);
+    Serial.print(baro_diff);
     Serial.print("m    ");
 
     // Set values to send
     strcpy(myData.latitude, gps_get_latitude());
     strcpy(myData.longitude, gps_get_longitude());
     strcpy(myData.altitude, gps_get_altitude());
+    myData.baro_alt = baro_diff;
 
     myData.message_counter = counter;
 
